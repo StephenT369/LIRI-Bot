95fc722ea8b774d09f096e09386fd04d4cc5142f
Stephen Thompson <smt3@hotmail.com>
2019-08-19 17:41:33 -0500

    bands

--- liri.js
+++ liri.js
@@ -1,46 +1,57 @@
-require('dotenv').config();
-var keys = require('./keys.js');
-//var spotify = new Spotify(keys.spotify);
-var axios = require('axios');
-var moment = require('moment');
-var fs = require('fs');
+require("dotenv").config();
+var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
 var command = process.argv[2];
 var request = process.argv;
-var requested = request.slice(3).join('%2B');
+var requested = request.slice(3).join("%2B");
 
-switch (command){
-case 'concert-this':
+switch (command) {
+  case "concert-this":
     var artist = requested;
     bands();
-break;
+    break;
 
-case 'spotify-this-song':
+  case "spotify-this-song":
+  var track = requested;
+  spotifyThis();  
+  break;
 
-break;
+  case "movie-this":
+    break;
 
-case 'movie-this':
-
-break;
-
-case 'do-what-it-says':
-
-break;
+  case "do-what-it-says":
+    break;
 };
 
-function bands (){
-var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+function bands() {
+  var queryURL =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
 
-axios.get(queryURL).then(
-    function(band){
-
-       var bandData = band.data;
-
-       for (i = 0; i < bandData.length; i++ ){
-        console.log('Name of the venue: ' + bandData[i].venue.name );
-        console.log('Venue location: ' + bandData[i].venue.city +','+ bandData[i].venue.region);
-        console.log('Date of the Event: ' + bandData[i].datetime);
-       };
-        
+  axios.get(queryURL).then(function(band) {
+    var bandData = band.data;
+    for (i = 0; i < bandData.length; i++) {
+      console.log("\nName of the venue: " + bandData[i].venue.name);
+      console.log(
+        "Venue location: " +
+          bandData[i].venue.city +
+          "," +
+          bandData[i].venue.region
+      );
+      console.log(
+        "Date of the Event: " +
+          moment(bandData[i].datetime).format("MM/DD/YYYY")
+      );
     }
-);
+  });
+};
+
+function spotifyThis(){
+spotify({})
 };
